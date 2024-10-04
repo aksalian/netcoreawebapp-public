2f73cdaa4a4b46ee709e02c76034c2262f994b13
aksalian <56423460+aksalian@users.noreply.github.com>
2024-10-04 23:05:54 +0530

    Update Program.cs

--- webapplication01/Program.cs
+++ webapplication01/Program.cs
@@ -1,7 +1,5 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World from Dev"); // + builder.Configuration["DisplayEnvironment"]);
-//app.MapGet("/", () => "Deployment triggered from " + builder.Configuration["DisplaySourceEnvironment"]  + builder.Configuration["DisplayTargetEnvironment"]);
-
+app.MapGet("/", () => "Hello World from " + builder.Configuration["DisplayEnvironment"]);
 app.Run();
